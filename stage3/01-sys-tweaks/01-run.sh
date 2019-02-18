RPi-Distro/pi-gen

commit 88e237e28de7b3ef91da56a32d18ce510b4e8183

Install published pynetworktables wheel from PyPI (#86)
Avoids having to create a (potentially incorrect) version.py manually.

Uses pip to install to generate and install the *.pyc (bytecode) files.

stage3/01-sys-tweaks/01-run.sh
@@ -44,8 +44,8 @@ wget -nc -nv -O allwpilib.tar.gz \
     https://github.com/wpilibsuite/allwpilib/archive/05d6660a6be0d37064a9fab44232b8379884ccab.tar.gz
 
 # pynetworktables
-wget -nc -nv -O pynetworktables.tar.gz \
-    https://github.com/robotpy/pynetworktables/archive/8a4288452be26e26dccad32980f46000e8d97928.tar.gz
+wget -nc -nv -O pynetworktables.whl \
+    https://files.pythonhosted.org/packages/source/p/pynetworktables/pynetworktables-2019.0.0-py3-none-any.whl
 
 # robotpy-cscore
 wget -nc -nv -O robotpy-cscore.tar.gz \
@@ -80,11 +80,6 @@ popd
 # allwpilib
 tar xzf "${DOWNLOAD_DIR}/allwpilib.tar.gz"
 mv allwpilib-* allwpilib
-
-# pynetworktables
-tar xzf "${DOWNLOAD_DIR}/pynetworktables.tar.gz"
-mv pynetworktables-* pynetworktables
-echo "__version__ = '2019.0.1'" > pynetworktables/ntcore/version.py
 
 # robotpy-cscore
 tar xzf "${DOWNLOAD_DIR}/robotpy-cscore.tar.gz"
@@ -275,12 +270,7 @@ popd
 
 #sh -c "cd ${EXTRACT_DIR}/pynetworktables && tar cf - networktables ntcore" | sh -c "cd ${ROOTFS_DIR}/usr/local/lib/python3.5/dist-packages/ && tar xf -"
 on_chroot << EOF
-pip3 install setuptools
-pushd /usr/src/pynetworktables
-python3 setup.py build
-python3 setup.py install
-python3 setup.py clean
-popd
+pip3 install /usr/src/pynetworktables.whl
 EOF
 
 #

wpilibsuite/FRCVision-pi-gen

commit c78f49914dfbe5f70ef3fa43c7e185d5f8d6661e

Unbreak pynetworktables (broken by #86)
The download URL was incorrect.

Add exit checks to make sure this causes a build failure if it happens again.

stage3/01-sys-tweaks/01-run.sh
@@ -37,27 +37,33 @@ wget -nc -nv \
 
 # opencv sources
 wget -nc -nv \
-    https://github.com/opencv/opencv/archive/3.4.4.tar.gz
+    https://github.com/opencv/opencv/archive/3.4.4.tar.gz \
+    || exit 1
 
 # allwpilib
 wget -nc -nv -O allwpilib.tar.gz \
-    https://github.com/wpilibsuite/allwpilib/archive/v2019.3.2.tar.gz
+    https://github.com/wpilibsuite/allwpilib/archive/v2019.3.2.tar.gz \
+    || exit 1
 
 # pynetworktables
 wget -nc -nv -O pynetworktables.whl \
-    https://files.pythonhosted.org/packages/source/p/pynetworktables/pynetworktables-2019.0.0-py3-none-any.whl
+    https://files.pythonhosted.org/packages/86/6d/b32c5232281a9c7a76822bb58d3803f8b1fe502954b08580a8425a899ea1/pynetworktables-2019.0.0-py3-none-any.whl \
+    || exit 1
 
 # robotpy-cscore
 wget -nc -nv -O robotpy-cscore.tar.gz \
-    https://github.com/robotpy/robotpy-cscore/archive/2019.1.0.tar.gz
+    https://github.com/robotpy/robotpy-cscore/archive/2019.1.0.tar.gz \
+    || exit 1
 
 # pybind11 submodule of robotpy-cscore
 wget -nc -nv -O pybind11.tar.gz \
-    https://github.com/pybind/pybind11/archive/v2.2.tar.gz
+    https://github.com/pybind/pybind11/archive/v2.2.tar.gz \
+    || exit 1
 
 # pixy2
 wget -nc -nv -O pixy2.tar.gz \
-    https://github.com/charmedlabs/pixy2/archive/2adc6caba774a3056448d0feb0c6b89855a392f4.tar.gz
+    https://github.com/charmedlabs/pixy2/archive/2adc6caba774a3056448d0feb0c6b89855a392f4.tar.gz \
+    || exit 1
 
 popd
 