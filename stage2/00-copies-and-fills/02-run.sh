0d82e3ba423c11c853d4bd1d38853555ef85e24f
GrantPerkins <gcperk20@gmail.com>
2021-01-06 16:16:23 -0500

    EOF block

--- stage2/00-copies-and-fills/02-run.sh
+++ stage2/00-copies-and-fills/02-run.sh
@@ -9,3 +9,7 @@ echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" > "${
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
 sudo apt-get update
+
+on_chroot << EOF
+pip3 install https://github.com/google-coral/pycoral/releases/download/release-frogfish/tflite_runtime-2.5.0-cp37-cp37m-linux_armv7l.whl
+EOF
