ce7443792e9dc467c16a53a34418a6a1cf42349b
GrantPerkins <gcperk20@gmail.com>
2021-01-06 19:55:33 -0500

    Add missing on_chroot

--- stage2/00-configure-apt/00-run.sh
+++ stage2/00-configure-apt/00-run.sh
@@ -3,8 +3,8 @@
 echo "adding repo for edgetpu"
 echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" > "${ROOTFS_DIR}/etc/apt/sources.list.d/coral-edgetpu.list"
 
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-
+on_chroot apt-key add - < files/edgetpu.gpg
 on_chroot << EOF
 sudo apt-get update
+pip3 install https://github.com/google-coral/pycoral/releases/download/release-frogfish/tflite_runtime-2.5.0-cp37-cp37m-linux_armv7l.whl
 EOF
--- stage2/00-configure-apt/01-run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-
-on_chroot << EOF
-pip3 install https://github.com/google-coral/pycoral/releases/download/release-frogfish/tflite_runtime-2.5.0-cp37-cp37m-linux_armv7l.whl
-EOF
