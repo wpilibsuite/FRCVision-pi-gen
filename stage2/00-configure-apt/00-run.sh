e5f31ce52e675874cb89717bfeab527575a760d1
GrantPerkins <gcperk20@gmail.com>
2021-01-06 19:50:36 -0500

    Echo for testing

--- stage2/00-configure-apt/00-run.sh
+++ stage2/00-configure-apt/00-run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 
+echo "adding repo for edgetpu"
 echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" > "${ROOTFS_DIR}/etc/apt/sources.list.d/coral-edgetpu.list"
 
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
--- stage2/00-configure-apt/01-run.sh
+++ stage2/00-configure-apt/01-run.sh
@@ -2,5 +2,4 @@
 
 on_chroot << EOF
 pip3 install https://github.com/google-coral/pycoral/releases/download/release-frogfish/tflite_runtime-2.5.0-cp37-cp37m-linux_armv7l.whl
-sudo apt-get update
 EOF
